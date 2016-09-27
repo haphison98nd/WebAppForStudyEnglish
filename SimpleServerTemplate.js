e7e446d4034d9fd8ff9d2ee51baa250102d35f0b
KentaroUeda <g2115005@fun.ac.jp>
2016-09-27 12:24:25 +0900

    Initial commit

--- SimpleServerTemplate.js
+++ SimpleServerTemplate.js
@@ -1,62 +1,3 @@
-/*
-var LISTEN_IP = '127.0.0.1'; 
-var LISTEN_PORT = 8086; 
-
-var DEFAULT_FILE = "default.html"; 
-
-var http = require('http'), 
-    fs = require('fs'); 
-
-//content-type を指定 
-function getContentType(fileName) { 
-    var extentsion = getExtension(fileName).toLowerCase(); 
-    var contentType = { 
-        'html': 'text/html', 
-        'htm' : 'text/htm', 
-        'css' : 'text/css', 
-        'js' : 'text/javaScript; charset=utf-8', 
-        'json' : 'application/json; charset=utf-8', 
-        'xml' : 'application/xml; charset=utf-8', 
-        'jpeg' : 'image/jpeg', 
-        'jpg' : 'image/jpg', 
-        'gif' : 'image/gif', 
-        'png' : 'image/png', 
-        'mp3' : 'audio/mp3', 
-        }; 
-        var contentType_value = contentType[extentsion]; 
-        if(contentType_value === undefined){ 
-            contentType_value = 'text/plain';}; 
-    return contentType_value; 
-} 
-
-//Web サーバーのロジック 
-var server  = http.createServer(); 
-server.on('request', 
-    function(request, response){ 
-        console.log('Requested Url:' + request.url); 
-        var requestedFile = request.url; 
-        requestedFile = (requestedFile.substring(requestedFile.length - 1, 1) === '/')?requestedFile + DEFAULT_FILE : requestedFile; 
-        console.log('Handle Url:' + requestedFile); 
-        console.log('File Extention:' + getExtension( requestedFile)); 
-        console.log('Content-Type:' + getContentType( requestedFile)); 
-        fs.readFile('.' + requestedFile,'binary', function (err, data) { 
-            if(err){ 
-                response.writeHead(404, {'Content-Type': 'text/plain'}); 
-                response.write('not found\n'); 
-                response.end();    
-            }else{ 
-                response.writeHead(200, {'Content-Type': getContentType(requestedFile)}); 
-                response.write(data, "binary"); 
-                response.end(); 
-            } 
-        }); 
-    } 
-); 
-
-server.listen(LISTEN_PORT, LISTEN_IP); 
-console.log('Server running at http://' + LISTEN_IP + ':' + LISTEN_PORT);
-*/
-
 var SimpleServerTemplate = function(){
     
     const getExtension = function (fileName) { 
@@ -123,7 +64,7 @@ var SimpleServerTemplate = function(){
                         response.write(data, 'binary'); 
                         response.end(); 
                     } 
-            }); 
+            });
         });
 
         server.listen(LISTEN_PORT, LISTEN_IP); 
@@ -131,9 +72,8 @@ var SimpleServerTemplate = function(){
 
     };
     
-    return {getContentType:getContentType, startServer:startServer};
+    return {startServer:startServer};
 };
-
 
 (function main (){
     const simpleServerTemplate = SimpleServerTemplate();
